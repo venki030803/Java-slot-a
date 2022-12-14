46f5b441aee84b6ebad31e6f0f9754bd7ac2e28e
venki030803 <113572846+venki030803@users.noreply.github.com>
2022-12-14 20:32:13 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class palindromeString {
+ public static void main(String[] args) {
+ Scanner in = new Scanner(System.in);
+ String str = in.nextLine(), reverseStr = "";
+ int strLength = str.length();
+ for (int i = (strLength - 1); i >=0; --i) {
+ reverseStr = reverseStr + str.charAt(i);}
+ if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
+ System.out.println(str + " is a Palindrome String.");}
+ else {
+ System.out.println(str + " is not a Palindrome String.");}
+ }
+}
